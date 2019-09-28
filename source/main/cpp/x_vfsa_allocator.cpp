d02eb735cc20f751f200284331e82c16f09cf3c7
Jurgen <jurgen.kluft@gmail.com>
2019-09-28 21:16:41 +0800

    * reducing size of xvpage_t * more const usage

--- source/main/cpp/x_vfsa_allocator.cpp
+++ source/main/cpp/x_vfsa_allocator.cpp
@@ -12,16 +12,16 @@ namespace xcore
         enum
         {
             INDEX_NIL     = 0xffffffff,
-			INDEX16_NIL   = 0xffff,
+            INDEX16_NIL   = 0xffff,
             PAGE_PHYSICAL = 1,
             PAGE_VIRTUAL  = 2,
         };
 
-		// Constraints:
-		// - maximum number of elements is 32768
-		// - minimum size of an element is 4 bytes
-		// - maximum page-size is 32768 * sizeof-element
-		// 
+        // Constraints:
+        // - maximum number of elements is 32768
+        // - minimum size of an element is 4 bytes
+        // - maximum page-size is 32768 * sizeof-element
+        //
         u32 m_next;
         u32 m_prev;
         u16 m_free_list;
@@ -37,11 +37,11 @@ namespace xcore
         bool is_virtual() const { return (m_flags & PAGE_VIRTUAL) == PAGE_VIRTUAL; }
         bool is_linked() const { return !(m_next == INDEX_NIL && m_prev == INDEX_NIL); }
 
-        void set_is_virtual() { m_flags = (m_flags & ~(PAGE_PHYSICAL|PAGE_VIRTUAL)) | PAGE_VIRTUAL; }
-        void set_is_physical() { m_flags = (m_flags & ~(PAGE_PHYSICAL|PAGE_VIRTUAL)) | PAGE_PHYSICAL; }
+        void set_is_virtual() { m_flags = (m_flags & ~(PAGE_PHYSICAL | PAGE_VIRTUAL)) | PAGE_VIRTUAL; }
+        void set_is_physical() { m_flags = (m_flags & ~(PAGE_PHYSICAL | PAGE_VIRTUAL)) | PAGE_PHYSICAL; }
     };
 
-    void init_page(xvpage_t* page, u32 page_size, u32 element_size)
+    void init_page(xvpage_t* const page, u32 const page_size, u32 const element_size)
     {
         page->m_next       = xvpage_t::INDEX_NIL;
         page->m_prev       = xvpage_t::INDEX_NIL;
@@ -53,24 +53,24 @@ namespace xcore
         page->m_elem_size  = element_size;
     }
 
-    static void inline page_unlink(xvpage_t* page)
+    static void inline page_unlink(xvpage_t* const page)
     {
         page->m_next = xvpage_t::INDEX_NIL;
         page->m_prev = xvpage_t::INDEX_NIL;
     }
 
-    static inline u32  indexof_elem(xvpage_t* page, void* page_base_address, void* elem) 
+    static inline u32 indexof_elem(xvpage_t* const page, void* const page_base_address, void* const elem)
     {
-        u32 const index = (u32)(((u64)elem - (u64)page_base_address) / page->m_elem_size); 
+        u32 const index = (u32)(((u64)elem - (u64)page_base_address) / page->m_elem_size);
         return index;
     }
-    static inline u32* indexto_elem(xvpage_t* page, void* page_base_address, u32 index)
+    static inline u32* indexto_elem(xvpage_t* const page, void* const page_base_address, u32 const index)
     {
         u32* elem = (u32*)((xbyte*)page_base_address + ((u64)index * (u64)page->m_elem_size));
         return elem;
     }
 
-    void* allocate_from_page(xvpage_t* page, void* page_base_address)
+    void* allocate_from_page(xvpage_t* const page, void* const page_base_address)
     {
         // if page == nullptr then first allocate a new page for this size
         // else use the page to allocate a new element.
@@ -80,7 +80,7 @@ namespace xcore
             u32  index        = page->m_free_list;
             u32* elem         = indexto_elem(page, page_base_address, index);
             page->m_free_list = elem[0];
-			page->m_elem_used++;
+            page->m_elem_used++;
             return (void*)elem;
         }
         else if (page->m_free_index < page->m_elem_total)
@@ -96,40 +96,40 @@ namespace xcore
         }
     }
 
-    void deallocate_from_page(xvpage_t* page, void* page_base_address, void* ptr)
+    void deallocate_from_page(xvpage_t* page, void* const page_base_address, void* const ptr)
     {
         u32  index        = indexof_elem(page, page_base_address, ptr);
         u32* elem         = indexto_elem(page, page_base_address, index);
         elem[0]           = page->m_free_list;
         page->m_free_list = index;
-		page->m_elem_used -= 1;
+        page->m_elem_used -= 1;
     }
 
-    static void      insert_in_list(xvpages_t* pages, u32& head, u32 page);
-    static void      remove_from_list(xvpages_t* pages, u32& head, u32 page);
+    static void insert_in_list(xvpages_t* pages, u32& head, u32 page);
+    static void remove_from_list(xvpages_t* pages, u32& head, u32 page);
 
     class xvpages_t
     {
     public:
-        void initialize(xvpage_t* pages, u32 pagesize, u32 pagecount, void* memory_base, u64 memory_range)
-		{
-            m_page_array = pages;
-            m_page_size = pagesize;
-            m_page_total_cnt = pagecount;
-			m_memory_base = memory_base;
-			m_memory_range = memory_range;
-            m_free_pages_physical_head = xvpage_t::INDEX_NIL;
+        xvpages_t(xvpage_t* pages, u32 pagesize, u32 pagecount, void* memory_base, u64 memory_range)
+        {
+            m_page_array                = pages;
+            m_page_size                 = pagesize;
+            m_page_total_cnt            = pagecount;
+            m_memory_base               = memory_base;
+            m_memory_range              = memory_range;
+            m_free_pages_physical_head  = xvpage_t::INDEX_NIL;
             m_free_pages_physical_count = 0;
-            m_free_pages_virtual_head = xvpage_t::INDEX_NIL;
-            m_free_pages_virtual_count = 0;
-		}
+            m_free_pages_virtual_head   = xvpage_t::INDEX_NIL;
+            m_free_pages_virtual_count  = 0;
+        }
 
         u64 memory_range() const { return m_memory_range; }
 
         void* allocate(u32& freelist, u32 allocsize);
         void  deallocate(u32& freelist, void* ptr);
 
-        xvpage_t* alloc_page(u32 allocsize)
+        xvpage_t* alloc_page(u32 const allocsize)
         {
             // Get a page from list of physical pages
             // If there are no free physical pages then take one from list of virtual pages and commit the page
@@ -144,14 +144,14 @@ namespace xcore
                     remove_from_list(this, m_free_pages_virtual_head, m_free_pages_virtual_head);
 
                     // Commit the virtual memory to physical memory
-                    xvirtual_memory* vmem = gGetVirtualMemory();
-                    void* address = get_base_address(ppage);
+                    xvirtual_memory* vmem    = gGetVirtualMemory();
+                    void*            address = get_base_address(ppage);
                     vmem->commit(address, m_page_size, 1);
                 }
             }
             else
             {
-                    // Get the page pointer and remove it from the list of physical pages
+                // Get the page pointer and remove it from the list of physical pages
                 ppage = indexto_page(m_free_pages_physical_head);
                 remove_from_list(this, m_free_pages_physical_head, m_free_pages_physical_head);
                 m_free_pages_physical_count -= 1;
@@ -166,7 +166,7 @@ namespace xcore
             return ppage;
         }
 
-        void      free_page(xvpage_t* ppage)
+        void free_page(xvpage_t* const ppage)
         {
             // If amount of physical pages crosses the maximum then decommit the memory of this page and add it to list of virtual pages
             // Otherwise add this page to the list of physical pages
@@ -177,29 +177,28 @@ namespace xcore
             }
             else
             {
-                xvirtual_memory* vmem = gGetVirtualMemory();
-                void* address = get_base_address(ppage);
+                xvirtual_memory* const vmem    = gGetVirtualMemory();
+                void* const            address = get_base_address(ppage);
                 vmem->decommit(address, m_page_size, 1);
                 ppage->set_is_virtual();
             }
-            
         }
 
-        xvpage_t* find_page(void* address) const
+        xvpage_t* find_page(void* const address) const
         {
             u32 const page_index = ((u64)address - (u64)m_memory_base) / m_page_size;
             return &m_page_array[page_index];
         }
 
-        void* idx2ptr(u64 index, u32 page_elem_cnt, u32 alloc_size) const
+        void* idx2ptr(u64 const index, u32 const page_elem_cnt, u32 const alloc_size) const
         {
-            u64 const page = index / page_elem_cnt;
-            u64 const elem = index - (page * page_elem_cnt);
-            void* ptr = (xbyte*)m_memory_base + (page * m_page_size) + (elem * alloc_size);
+            u64 const   page = index / page_elem_cnt;
+            u64 const   elem = index - (page * page_elem_cnt);
+            void* const ptr  = (xbyte*)m_memory_base + (page * m_page_size) + (elem * alloc_size);
             return ptr;
         }
 
-        u64   ptr2idx(void* ptr, u32 page_elem_cnt, u32 alloc_size) const
+        u64 ptr2idx(void* const ptr, u32 const page_elem_cnt, u32 const alloc_size) const
         {
             u64 const page = ((u64)ptr - (u64)m_memory_base) / m_page_size;
             u64 const base = (u64)m_memory_base + (page * m_page_size);
@@ -207,75 +206,76 @@ namespace xcore
             return elem;
         }
 
-        void*     get_base_address(xvpage_t* page) const
+        void* get_base_address(xvpage_t* const page) const
         {
             u32 const page_index = indexof_page(page);
             return (xbyte*)m_memory_base + (page_index * m_page_size);
         }
 
-        xvpage_t* next_page(xvpage_t* page)
+        xvpage_t* next_page(xvpage_t* const page)
         {
-            u32 next = page->m_next;
+            u32 const next = page->m_next;
             return &m_page_array[next];
         }
-        xvpage_t* prev_page(xvpage_t* page)
+        xvpage_t* prev_page(xvpage_t* const page)
         {
-            u32 prev = page->m_prev;
+            u32 const prev = page->m_prev;
             return &m_page_array[prev];
         }
 
-        xvpage_t* indexto_page(u32 page) const
+        xvpage_t* indexto_page(u32 const page) const
         {
             ASSERT(page < m_page_total_cnt);
             return &m_page_array[page];
         }
-        u32       indexof_page(xvpage_t* ppage) const
+        u32 indexof_page(xvpage_t* const ppage) const
         {
-            if (ppage == nullptr) return xvpage_t::INDEX_NIL;
-            u32 page = (u32)(ppage - &m_page_array[0]);
+            if (ppage == nullptr)
+                return xvpage_t::INDEX_NIL;
+            u32 const page = (u32)(ppage - &m_page_array[0]);
             return page;
         }
 
-        void*     m_memory_base;
-        u64       m_memory_range;
-        u32       m_page_size;
-        u32       m_page_total_cnt;
-        u32       m_free_pages_physical_head;
-        u32       m_free_pages_physical_count;
-        u32       m_free_pages_virtual_head;
-        u32       m_free_pages_virtual_count;
-        xvpage_t* m_page_array;
+        void* const     m_memory_base;
+        u64 const       m_memory_range;
+        u32 const       m_page_size;
+        u32 const       m_page_total_cnt;
+        u32             m_free_pages_physical_head;
+        u32             m_free_pages_physical_count;
+        u32             m_free_pages_virtual_head;
+        u32             m_free_pages_virtual_count;
+        xvpage_t* const m_page_array;
     };
 
-    static inline void      insert_in_list(xvpages_t* pages, u32& head, u32 page)
+    static inline void insert_in_list(xvpages_t* pages, u32& head, u32 page)
     {
         if (head == xvpage_t::INDEX_NIL)
         {
-            xvpage_t* ppage = &pages->m_page[page];
-            ppage->m_next = page;
-            ppage->m_prev = page;
-            head = page;
+            xvpage_t* const ppage = &pages->m_page[page];
+            ppage->m_next         = page;
+            ppage->m_prev         = page;
+            head                  = page;
         }
         else
         {
-            xvpage_t* phead = &pages->m_page[head];
-            xvpage_t* pnext = &pages->m_page[phead->m_next];
-            xvpage_t* ppage = &pages->m_page[page];
-            ppage->m_prev = head;
-            ppage->m_next = phead->m_next;
-            phead->m_next = page;
-            pnext->m_prev = page;
+            xvpage_t* const phead = &pages->m_page[head];
+            xvpage_t* const pnext = &pages->m_page[phead->m_next];
+            xvpage_t* const ppage = &pages->m_page[page];
+            ppage->m_prev         = head;
+            ppage->m_next         = phead->m_next;
+            phead->m_next         = page;
+            pnext->m_prev         = page;
         }
     }
 
-    static inline void      remove_from_list(xvpages_t* pages, u32& head, u32 page)
+    static inline void remove_from_list(xvpages_t* pages, u32& head, u32 page)
     {
-        xvpage_t* phead = &pages->m_page[head];
-        xvpage_t* ppage = &pages->m_page[page];
-        xvpage_t* pprev = &pages->m_page[ppage->m_prev];
-        xvpage_t* pnext = &pages->m_page[ppage->m_next];
-        pprev->m_next = ppage->m_next;
-        pnext->m_prev = ppage->m_prev;
+        xvpage_t* const phead = &pages->m_page[head];
+        xvpage_t* const ppage = &pages->m_page[page];
+        xvpage_t* const pprev = &pages->m_page[ppage->m_prev];
+        xvpage_t* const pnext = &pages->m_page[ppage->m_next];
+        pprev->m_next         = ppage->m_next;
+        pnext->m_prev         = ppage->m_prev;
         page_unlink(ppage);
 
         if (phead == ppage)
@@ -295,17 +295,17 @@ namespace xcore
         // Using the page allocate a new element
         // return pointer to element
         // If page is full remove it from the free list
-        u32 page = xvpage_t::INDEX_NIL;
+        u32       page  = xvpage_t::INDEX_NIL;
         xvpage_t* ppage = nullptr;
         if (page_list == xvpage_t::INDEX_NIL)
         {
             ppage = alloc_page(allocsize);
-            page = indexof_page(ppage);
+            page  = indexof_page(ppage);
             insert_in_list(this, page_list, page);
         }
         else
         {
-            page = page_list;
+            page  = page_list;
             ppage = indexto_page(page);
         }
 
@@ -329,7 +329,7 @@ namespace xcore
         // If page is now empty, decide to deallocate this page
         // When deallocating this page, remove it from the free list
         xvpage_t* ppage = find_page(ptr);
-        u32 const page = indexof_page(ppage);
+        u32 const page  = indexof_page(ppage);
         deallocate_from_page(ppage, get_base_address(ppage), ptr);
         if (ppage->is_empty())
         {
@@ -343,22 +343,20 @@ namespace xcore
         {
             insert_in_list(this, freelist, page);
         }
-    }    
+    }
 
-	// An object that can alloc/free pages from a memory range
-	xvpages_t*	gCreateVPages(xalloc* main_allocator, u64 memoryrange)
+    // An object that can alloc/free pages from a memory range
+    xvpages_t* gCreateVPages(xalloc* main_allocator, u64 memoryrange)
     {
-        xvpages_t* vpages = main_allocator->construct<xvpages_t>(es, allocsize);
-
         xvirtual_memory* vmem = gGetVirtualMemory();
-        u32 pagesize;
-        void* memory_base = nullptr;
+        u32              pagesize;
+        void*            memory_base = nullptr;
         vmem->reserve(memoryrange, pagesize, 0, memory_base);
 
         u32 const pagecount = memoryrange / pagesize;
         xvpage_t* pagearray = main_allocator->allocate(sizeof(xvpage_t) * pagecount, sizeof(void*));
 
-        vpages->initialize(pagearray, pagesize, pagecount, memory_base, memory_range);
+        xvpages_t* vpages = main_allocator->construct<xvpages_t>(pagearray, pagesize, pagecount, memory_base, memory_range);
 
         return vpages;
     }
@@ -371,31 +369,22 @@ namespace xcore
     public:
         inline xvfsa(xvpages_t* pages, u32 allocsize) : m_pages(pages), m_pages_freelist(xvpage_t::INDEX_NIL), m_alloc_size(allocsize) {}
 
-		virtual u32 size() const { return m_alloc_size; }
+        virtual u32 size() const { return m_alloc_size; }
 
-        virtual void* allocate()
-        {
-            return m_pages->allocate(m_pages_freelist, m_alloc_size);
-        }
+        virtual void* allocate() { return m_pages->allocate(m_pages_freelist, m_alloc_size); }
 
-        virtual void deallocate(void* ptr)
-        {
-            return m_pages->deallocate(m_pages_freelist, ptr);
-        }
+        virtual void deallocate(void* ptr) { return m_pages->deallocate(m_pages_freelist, ptr); }
 
-		virtual void release()
-		{
-			
-		}
+        virtual void release() {}
 
-		XCORE_CLASS_PLACEMENT_NEW_DELETE
+        XCORE_CLASS_PLACEMENT_NEW_DELETE
     protected:
-        xvpages_t* m_pages;
-        u32        m_pages_freelist;
-        u32        m_alloc_size;
+        xvpages_t* const m_pages;
+        u32              m_pages_freelist;
+        u32 const        m_alloc_size;
     };
 
-    xfsa* gCreateVMemBasedFsa(xalloc* main_allocator, xvpages_t* vpages, u32 allocsize) 
+    xfsa* gCreateVMemBasedFsa(xalloc* main_allocator, xvpages_t* vpages, u32 allocsize)
     {
         xvfsa* fsa = main_allocator->construct<xvfsa>(vpages, allocsize);
         return fsa;
@@ -407,45 +396,30 @@ namespace xcore
     class xvfsa_dexed : public xfsadexed
     {
     public:
-        inline xvfsa_dexed(xvpages_t* pages, u32 allocsize) : m_pages(pages), m_pages_freelist(xvpage_t::INDEX_NIL), m_alloc_size(allocsize) 
-		{
-			m_page_elem_cnt = pages->m_page_size / allocsize;
-		}
+        inline xvfsa_dexed(xvpages_t* pages, u32 allocsize) : m_pages(pages), m_pages_freelist(xvpage_t::INDEX_NIL), m_alloc_size(allocsize), m_page_elem_cnt(pages->m_page_size / allocsize) {}
 
-		virtual u32 size() const { return m_alloc_size; }
+        virtual u32 size() const { return m_alloc_size; }
 
-        virtual void* allocate()
-        {
-            return m_pages->allocate(m_pages_freelist, m_alloc_size);
-        }
+        virtual void* allocate() { return m_pages->allocate(m_pages_freelist, m_alloc_size); }
 
-        virtual void deallocate(void* ptr)
-        {
-            return m_pages->deallocate(m_pages_freelist, ptr);
-        }
+        virtual void deallocate(void* ptr) { return m_pages->deallocate(m_pages_freelist, ptr); }
 
-        virtual void* idx2ptr(u32 index) const
-        {
-            return m_pages->idx2ptr(index, m_page_elem_cnt, m_alloc_size);
-        }
+        virtual void* idx2ptr(u32 index) const { return m_pages->idx2ptr(index, m_page_elem_cnt, m_alloc_size); }
 
-        virtual u32   ptr2idx(void* ptr) const
+        virtual u32 ptr2idx(void* ptr) const
         {
             u64 index = m_pages->ptr2idx(ptr, m_page_elem_cnt, m_alloc_size);
             return (u32)index;
         }
 
-		virtual void release()
-		{
-			
-		}
+        virtual void release() {}
 
-		XCORE_CLASS_PLACEMENT_NEW_DELETE
+        XCORE_CLASS_PLACEMENT_NEW_DELETE
     protected:
-        xvpages_t* m_pages;
-        u32        m_pages_freelist;
-        u32        m_alloc_size;
-		u32		   m_page_elem_cnt;
+        xvpages_t* const m_pages;
+        u32              m_pages_freelist;
+        u32 const        m_alloc_size;
+        u32 const        m_page_elem_cnt;
     };
 
     // Constraints:
--- source/main/cpp/x_virtual_allocator.cpp
+++ source/main/cpp/x_virtual_allocator.cpp
@@ -1,12 +1,41 @@
 #include "xbase/x_target.h"
 #include "xbase/x_debug.h"
 #include "xbase/x_allocator.h"
+#include "xbase/x_btree.h"
 
 #include "xvmem/x_virtual_allocator.h"
 #include "xvmem/x_virtual_memory.h"
 
 namespace xcore
 {
+    // Using btree32 for the medium size allocators to track allocations.
+    // The key-value implementation would be like this:
+    struct node_t
+    {
+        u32 m_addr;      // (m_addr * size step) + base addr
+        u32 m_flags;     // Allocated, Free, Locked
+        u32 m_addr_prev; // previous node in memory, can be free, can be allocated
+        u32 m_addr_next; // next node in memory, can be free, can be allocated
+    };
+
+    class xvmem_alloc_kv : public xbtree_kv
+    {
+        xfsadexer* m_fsa;
+
+    public:
+        virtual u64 get_key(u32 value) const
+        {
+            // The key is the address
+            node_t* node = m_fsa->idx2ptr(value);
+            return node->m_addr;
+        }
+
+        virtual void set_key(u32 value, u64 key)
+        {
+            // Do not need to do anything since the key is already set
+        }
+    };
+
     class xvmem_allocator : public xalloc
     {
     public:
@@ -27,26 +56,26 @@ namespace xcore
         xfsa** m_fvsa_alloc;
 
         // Small allocator, 1 KB < size <= 4KB
-        s32    m_fsa_min_size;   // 1KB
-        s32    m_fsa_step_size;  // 16 (gives us 192 allocators)
-        s32    m_fsa_max_size;   // 4KB
-        void*  m_fsa_mem_base;   // A memory base pointer
-        u64    m_fsa_mem_range;  // 256 MB
+        s32    m_fsa_min_size;  // 1KB
+        s32    m_fsa_step_size; // 16 (gives us 192 allocators)
+        s32    m_fsa_max_size;  // 4KB
+        void*  m_fsa_mem_base;  // A memory base pointer
+        u64    m_fsa_mem_range; // 256 MB
         u32    m_fsa_page_size; // 64 KB (gives us 4KB pages)
-        xbyte* m_fsa_alloc_map;  // 4 KB
+        xbyte* m_fsa_alloc_map; // 4 KB
         xfsa** m_fsa_alloc;
 
         // Medium allocator
-        s32     m_med_min_size; // 4KB
-        s32     m_med_max_size; // 32MB
-        void*   m_med_mem_base; // A memory base pointer
+        s32     m_med_min_size;  // 4KB
+        s32     m_med_max_size;  // 32MB
+        void*   m_med_mem_base;  // A memory base pointer
         u64     m_med_mem_range; // 768 MB
         xalloc* m_med_allocator;
 
         // Large allocator
         s32     m_large_min_size; // 32MB
         void*   m_large_mem_base; // A memory base pointer
-        u64     m_large_mem_range; 
+        u64     m_large_mem_range;
         xalloc* m_large_allocator;
     };
 
@@ -108,16 +137,16 @@ namespace xcore
     {
         if (is_in_memrange(m_fvsa_mem_base, m_fvsa_mem_range, ptr))
         {
-            s32 const page_index = (s32)(((u64)ptr - (u64)m_fvsa_mem_base) / m_fvsa_page_size);
-            s32 alloc_index = m_fvsa_alloc_map[page_index];
-            xfsa* allocator = m_fvsa_alloc[alloc_index];
+            s32 const page_index  = (s32)(((u64)ptr - (u64)m_fvsa_mem_base) / m_fvsa_page_size);
+            s32       alloc_index = m_fvsa_alloc_map[page_index];
+            xfsa*     allocator   = m_fvsa_alloc[alloc_index];
             allocator->deallocate(ptr);
         }
         else if (is_in_memrange(m_fsa_mem_base, m_fsa_mem_range, ptr))
         {
-            s32 const page_index = (s32)(((u64)ptr - (u64)m_fsa_mem_base) / m_fsa_page_size);
-            s32 alloc_index = m_fvsa_alloc_map[page_index];
-            xfsa* allocator = m_fsa_alloc[alloc_index];
+            s32 const page_index  = (s32)(((u64)ptr - (u64)m_fsa_mem_base) / m_fsa_page_size);
+            s32       alloc_index = m_fvsa_alloc_map[page_index];
+            xfsa*     allocator   = m_fsa_alloc[alloc_index];
             allocator->deallocate(ptr);
         }
         else if (is_in_memrange(m_med_mem_base, m_med_mem_range, ptr))
@@ -134,4 +163,4 @@ namespace xcore
         }
     }
 
-};
+}; // namespace xcore
